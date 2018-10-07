2ada049323c1b62cba9fa007700156794bb3ebd7
YLiu6o <37677668+YLiu6o@users.noreply.github.com>
2018-10-07 11:26:51 +0800

    Update main.py

--- hw02/main.py
+++ hw02/main.py
@@ -1,1 +1,102 @@
 
+from turtle import *
+
+hideturtle()
+penup()
+left(180)
+forward(700)
+right(90)
+forward(395)
+right(180)
+pendown()
+
+
+color("red")
+begin_fill()
+
+forward(780)
+left(90)
+forward(1300)
+left(90)
+forward(780)
+left(90)
+forward(1300)
+left(90)
+forward(780)
+
+end_fill()
+
+penup()
+left(180)
+forward(620)
+right(90)
+forward(85)
+pendown()
+
+
+color("yellow")
+begin_fill()
+for x in range(1,6):
+    forward(200)
+    right(144)
+end_fill()
+
+
+penup()
+forward(300)
+left(90)
+forward(60)
+pendown()
+
+color("yellow")
+begin_fill()
+for x in range(1,6):
+    forward(80)
+    right(144)
+end_fill()
+
+
+penup()
+left(180)
+forward(80)
+left(90)
+forward(40)
+pendown()
+
+color("yellow")
+begin_fill()
+for x in range(1,6):
+    forward(80)
+    right(144)
+end_fill()
+
+
+penup()
+right(90)
+forward(120)
+pendown()
+
+color("yellow")
+begin_fill()
+for x in range(1,6):
+    forward(80)
+    right(144)
+end_fill()
+
+
+penup()
+right(45)
+forward(150)
+pendown()
+
+color("yellow")
+begin_fill()
+for x in range(1,6):
+    forward(80)
+    right(144)
+end_fill()
+
+
+ 
+
+done()
