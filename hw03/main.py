692c8049cf29d2f7e748827eae0aa23e1dc04257
YLiu6o <37677668+YLiu6o@users.noreply.github.com>
2018-10-21 20:20:01 +0800

    Update main.py

--- hw03/main.py
+++ hw03/main.py
@@ -1,1 +1,35 @@
+class Rectangle(object):
+    def __init__(self, width, height):
+        self.width =width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+
+class Square(Rectangle):
+
+    def __init__(self, width):
+        self.width  = width
+        self.height = width
+
+
+
+class Circle(object):
+    def __init__(self, radius):
+        self.width =radius
+
+    def area(self):
+        return (3.14*self.width)/2
+
+
+class Ellipse(Rectangle):
+    def area(self):
+        return 3.14*(self.width/2)*(self.height/2)
+
+    
+
+def compute_area(shapes):
+    return sum(x.area()for x in shapes)
+
 
